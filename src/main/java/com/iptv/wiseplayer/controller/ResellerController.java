84464e104330d53248ba4b44a2520ddae535325f
Shubham Bangwal <shubhambangwal@pearlorganisation.com>
2026-04-11 11:47:08 +0530

    fixed build issues

--- src/main/java/com/iptv/wiseplayer/controller/ResellerController.java
+++ src/main/java/com/iptv/wiseplayer/controller/ResellerController.java
@@ -20,6 +20,7 @@ import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
