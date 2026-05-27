c9222b4d34416b46cbd0230082e9b78fb6fae50f
shubham <shubhambangwal@pearlorgansiation.com>
2026-05-27 10:31:08 +0530

    updated the flow to fix some bugs

--- src/main/java/com/iptv/wiseplayer/controller/SubResellerController.java
+++ src/main/java/com/iptv/wiseplayer/controller/SubResellerController.java
@@ -78,7 +78,7 @@ public class SubResellerController {
 
     @PostMapping("/user")
     @Operation(summary = "Create End User", description = "Register an end user device under this sub-reseller")
-    public ResponseEntity<DeviceRegistrationResponse> createEndUser(
+    public ResponseEntity<java.util.Map<String, Object>> createEndUser(
             @Valid @RequestBody DeviceRegistrationRequest request) {
         return ResponseEntity.ok(resellerService.createEndUser(getCurrentSubResellerId(), request));
     }
