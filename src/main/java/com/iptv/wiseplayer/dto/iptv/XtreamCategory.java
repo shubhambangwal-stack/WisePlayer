339676358a7b5afcfe28594f8388d0a2770455b3
shubham <shubhambangwal@pearlorgansiation.com>
2026-08-06 14:04:24 +0530

    updated the json

--- src/main/java/com/iptv/wiseplayer/dto/iptv/XtreamCategory.java
+++ src/main/java/com/iptv/wiseplayer/dto/iptv/XtreamCategory.java
@@ -1,7 +1,13 @@
 package com.iptv.wiseplayer.dto.iptv;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class XtreamCategory {
     @JsonProperty("category_id")
@@ -10,6 +16,19 @@ public class XtreamCategory {
     private String categoryName;
     @JsonProperty("parent_id")
     private int parentId;
+
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<>();
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
 
     public String getCategoryId() {
         return categoryId;
--- src/main/java/com/iptv/wiseplayer/dto/iptv/XtreamVodStream.java
+++ src/main/java/com/iptv/wiseplayer/dto/iptv/XtreamVodStream.java
@@ -1,7 +1,13 @@
 package com.iptv.wiseplayer.dto.iptv;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class XtreamVodStream {
     @JsonProperty("num")
@@ -24,6 +30,19 @@ public class XtreamVodStream {
     private String containerExtension;
     @JsonProperty("direct_source")
     private String directSource;
+
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<>();
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
 
     public int getNum() {
         return num;
